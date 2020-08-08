ab6ce50d7f5180cdd78690b8b15b4cf23c28b638
Katherine Jones <kejone1414@gmail.com>
2020-08-08 11:25:51 -0400

    feat: added passport config

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,13 +5,13 @@ const validateRegisterInput = require("../../validation/register");
 
 // Defining methods for the Users Controller
 module.exports = {
-  findByEmail: function(request, response) {
+  findByEmail: function (request, response) {
     db.Users
       .findOne({ email: request.params.email })
       .then(dbModel => response.json(dbModel))
       .catch(error => response.status(422).json(error));
   },
-  create: function(request, response) {
+  create: function (request, response) {
     const { errors, isValid } = validateRegisterInput(req.body);
 
     if (!isValid) {
@@ -27,11 +27,11 @@ module.exports = {
           db.Users
             .create(request.body)
             .then(dbModel => response.json(dbModel))
-            .catch(error => response.status(422).json(error)); 
+            .catch(error => response.status(422).json(error));
         };
       });
   },
-  update: function(request, response) {
+  update: function (request, response) {
     db.Users
       .findOneAndUpdate({ _id: request.body._id }, request.body)
       .then(dbModel => response.json(dbModel))
