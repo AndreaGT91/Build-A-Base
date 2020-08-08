df75078ab274da78b85776019d3dfbfee1225c6d
Katherine Jones <kejone1414@gmail.com>
2020-08-08 10:41:00 -0400

    feat: decided not to move forward with okta; reset server and package.json to master, installed packages for using passport and jwt

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -7,8 +5,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { pid } = process;
-const session = require('express-session');
-const { ExpressOIDC } = require('@okta/oidc-middleware');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -21,31 +17,13 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Express sessions for user authentication
-app.use(session({
-  secret: APP_SECRET,
-  resave: true,
-  saveUninitialized: false
-}));
-
-const oidc = new ExpressOIDC({
-  issuer: OKTA_DOMAIN,
-  client_id: CLIENT_ID,
-  client_secret: CLIENT_SECRET,
-  redirect_uri: 'http://localhost:3000/authorization-code/callback',
-  scope: 'openid profile'
-});
-
-// ExpressOIDC attaches handlers for the /login and /authorization-code/callback routes
-app.use(oidc.router);
-
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/build-a-base",
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
 );
 
 // Start the API server
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`PID: ${pid}\n`);
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
