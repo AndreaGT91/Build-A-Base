dead6ee581d71a7fce81a982c8de3ff8c5af892f
Andrea Bentley <andrea@bentleytech.com>
2020-08-08 18:07:43 -0400

    Removed test code, added back default route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// TODO: Temporary, just for testing
-import API from "./utils/API";
-
 function App() {
-  // TODO: Temporary, just for testing
-  API.readSpreadsheet("/Users/andreabentley/Documents/github/Build-A-Base/sample-data/1010-Sample-Information.xlsx");
-
   return (
     <Router>
         <Switch>
