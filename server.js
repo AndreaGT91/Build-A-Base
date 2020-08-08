a0b787fd0f29ea4c7daf54e0d723f179e0e60d42
Katherine Jones <kejone1414@gmail.com>
2020-08-08 11:47:57 -0400

    feat: added passport to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { pid } = process;
@@ -23,6 +24,12 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // Add routes, both API and view
 app.use(routes);
