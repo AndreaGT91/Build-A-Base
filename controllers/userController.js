f366a2a36a50554e45404aa5ac037d77fa81a5f0
Katherine Jones <kejone1414@gmail.com>
2020-08-08 11:19:50 -0400

    feat: updated controller for registration validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const db = require("../models");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 // Defining methods for the Users Controller
 module.exports = {
   findByEmail: function(request, response) {
@@ -9,10 +12,24 @@ module.exports = {
       .catch(error => response.status(422).json(error));
   },
   create: function(request, response) {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     db.Users
-      .create(request.body)
-      .then(dbModel => response.json(dbModel))
-      .catch(error => response.status(422).json(error));
+      .findOne({ email: request.body.email })
+      .then(user => {
+        if (user) {
+          return res.status(400).json({ email: "Email already exists" });
+        } else {
+          db.Users
+            .create(request.body)
+            .then(dbModel => response.json(dbModel))
+            .catch(error => response.status(422).json(error)); 
+        };
+      });
   },
   update: function(request, response) {
     db.Users
