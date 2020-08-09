e9a807c2e8116c91c15d7f003274738b13fe08ff
Andrea Bentley <andrea@bentleytech.com>
2020-08-09 18:20:27 -0400

    Updated xlsx controller to correct headings with invalid characters

--- controllers/xlsxController.js
+++ controllers/xlsxController.js
@@ -5,17 +5,33 @@ module.exports = {
   convertXLSX: function(request, response) {
     if (fs.existsSync(request.body.filename)) {
       const workbook = XLSX.readFile(request.body.filename, { dateNF:'yyyy"-"mm"-"dd' });
+      // For MVP, we are only handling first sheet in a multi-sheet workbook;
+      // Also assuming that there is a header row
+      // let result = {};
+      // let sheet;
+    
+      // workbook.SheetNames.forEach(sheetName => {
+      //   sheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header:1, raw:false});
+      //   if (sheet.length) {
+      //     result[sheetName] = sheet;
+      //   };
+      // });
       let result = [];
 
-      // TODO: Evaluate headers - remove "." and "$"
-      // newField.fieldName = key.replace(/\./g, ""); // No "." allowed in field names
-      // if (newField.fieldName[0] === "$") {
-      //   newField.fieldName = newField.fieldName.substring(1); // Can't start with "$"
-      // }
-
-      
       if (workbook.SheetNames.length > 0) {
-        result = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {raw:false});
+        result = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header:1, raw:false});
+
+        // First object in array is column headers. Need to ensure none begin with "$" or contain "."s
+        // These will be the field names in MongoDB, and those are illegal characters
+        if (result.length > 0) {
+          result[0].forEach(header => {
+            header = header.replace(/\./g, ""); // Remove all "."
+
+            if (header[0] === "$") {
+              header = header.substring(1); // Remove "$" if in first position
+            };
+          });
+        };
       };
 
       response.json(result);
