1b896a771ce755f8a6f961d6d2e5aab52f6c32b0
Katherine Jones <kejone1414@gmail.com>
2020-08-10 22:32:39 -0400

    fix: debugging

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,6 +5,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
+    console.log(userData)
     axios
         .post("/api/user", userData)
         .then(res => history.push("/Login")) // re-direct to login on successful register
