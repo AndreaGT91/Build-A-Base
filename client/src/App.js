4720b764edf65288538468d9484ea507c3b82f04
Katherine Jones <kejone1414@gmail.com>
2020-08-11 00:05:51 -0400

    feat: private route for dashboard added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux"
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import NavBar from "./components/NavBar";
 import SectionOne from "./components/SectionOne";
 import SectionTwo from "./components/SectionTwo";
@@ -15,6 +18,7 @@ import MyDatabase from "./pages/MyDatabase";
 import { Parallax } from "react-parallax";
 import "./App.scss";
 import store from "./store"
+import PrivateRoute from "./components/private-route/PrivateRoute";
 
 
 import FirstImage from "./images/1.png"
@@ -30,6 +34,29 @@ const inlineStyle = {
   padding: '20px',
 }
 
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = JSON.parse(localStorage.jwtToken);
+  setAuthToken(token);
+
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./Login";
+  }
+}
+
 function App() {
   return (
     <div className="div1">
@@ -41,7 +68,7 @@ function App() {
               <Route exact path="/Login"><Login /></Route>
               <Route exact path="/SignUp"><SignUp /></Route>
               <Route exact path="/Home"><Home /></Route>
-              <Route exact path="/Dashboard"><Dashboard /></Route>
+              <PrivateRoute exact path="/Dashboard" component={Dashboard} />
               <Route exact path="/MyDatabase"><MyDatabase /></Route>
 
               <Route exact path="/*"><Home /></Route>
